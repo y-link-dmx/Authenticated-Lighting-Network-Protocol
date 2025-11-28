f6350545b6cf8cd6b45eb34181553c09c50c7085
kristofferous <kristoffer@nerskogen.com>
2025-11-28 10:40:54 +0100

    fix embedded build script

--- scripts/build_embedded_cpp.sh
+++ scripts/build_embedded_cpp.sh
@@ -6,12 +6,16 @@ DIST="$ROOT_DIR/dist/embedded"
 mkdir -p "$DIST"
 
 cd "$ROOT_DIR"
+./scripts/build_c.sh
 
 EMBEDDED_FLAGS="-DALPINE_EMBEDDED -std=c++17 -Wall -Wextra -Werror -fno-exceptions \
 -fno-rtti -fno-threadsafe-statics -fno-use-cxa-atexit -Os -ffunction-sections \
 -fdata-sections -fno-common -fno-stack-protector"
 
+VERSION=$(cat "$ROOT_DIR/VERSION")
+
 g++ $EMBEDDED_FLAGS -Ibindings/cpp -Ibindings/c \
-  bindings/cpp/embedded_test.cpp -o "$DIST/embedded_test"
+  bindings/cpp/embedded_test.cpp -L"$ROOT_DIR/dist/c" "-lalpine-$VERSION" \
+  -o "$DIST/embedded_test"
 
 echo "Embedded C++ binding built successfully (--embedded mode)."
